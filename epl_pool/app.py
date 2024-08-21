3c8c7ba2b12c1d6502e311bb69f55d6fb1b4165f
thayer6 <casey.thayer6@gmail.com>
2024-08-21 15:43:22 -0700

    feat: setup for 24-25 season

--- epl_pool/app.py
+++ epl_pool/app.py
@@ -4,58 +4,52 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 owner_teams = {
-    "Arsenal": "Casey",
-    "Aston Villa": "Casey",
-    "Bournemouth": "Jason ⭐️",
-    "Brentford": "CB",
-    "Brighton": "Megan ⭐️",
-    "Burnley": "Neo ⭐️⭐️",
+    "Arsenal": "Neo ⭐️⭐️",
+    "Aston Villa": "Neo ⭐️⭐️",
+    "Bournemouth": "Jason ⭐️⭐️",
+    "Brentford": "Jason ⭐️⭐️",
+    "Brighton": "CB",
     "Chelsea": "CB",
-    "Crystal Palace": "Jason ⭐️",
-    "Everton": "Megan ⭐️",
-    "Fulham": "CB",
-    "Liverpool": "Neo ⭐️⭐️",
-    "Luton Town": "CB",
-    "Manchester City": "Jason ⭐️",
-    "Manchester Utd": "Megan ⭐️",
-    "Newcastle Utd": "Neo ⭐️⭐️",
-    "Nott'ham Forest": "Neo ⭐️⭐️",
-    "Sheffield Utd": "Casey",
-    "Tottenham": "Jason ⭐️",
+    "Crystal Palace": "Megan ⭐️",
+    "Everton": "CB",
+    "Fulham": "Neo ⭐️⭐️",
+    "Liverpool": "Casey",
+    "Manchester City": "Megan ⭐️",
+    "Manchester Utd": "Casey",
+    "Newcastle Utd": "Jason ⭐️⭐️",
+    "Nott'ham Forest": "Megan ⭐️",
+    "Tottenham": "Jason ⭐️⭐️",
     "West Ham": "Casey",
     "Wolves": "Megan ⭐️",
+    "Ipswich Town": "Neo ⭐️⭐️",
+    "Leicester City": "CB",
+    "Southampton": "Casey",
 }
 
 owner_team_data = {
     "Squad": [key for key in owner_teams.keys()],
-    "Owner": [val for val in owner_teams.values()]
+    "Owner": [val for val in owner_teams.values()],
 }
 
-@app.route('/', methods = ("POST", "GET"))
+
+@app.route("/", methods=("POST", "GET"))
 def html_table():
     owner_table = pd.DataFrame(data=owner_team_data)
     table_df = pd.read_html("https://fbref.com/en/comps/9/Premier-League-Stats")[0]
     merged_table = pd.merge(table_df, owner_table, on="Squad")
-    extra_columns = [
-        "Last 5", "Attendance", "Top Team Scorer", "Goalkeeper", "Notes"
-    ]
+    extra_columns = ["Last 5", "Attendance", "Top Team Scorer", "Goalkeeper", "Notes"]
     columns_to_drop = [col for col in extra_columns if col in merged_table.columns]
-    final_table = merged_table.drop(
-        columns=columns_to_drop
+    final_table = merged_table.drop(columns=columns_to_drop)
+    owner_table = (
+        final_table.groupby(["Owner"])
+        .Pts.sum()
+        .sort_values(ascending=False)
+        .reset_index()
     )
-    owner_table = final_table.groupby(["Owner"]).Pts.sum().sort_values(ascending=False).reset_index()
     return render_template(
-            'index.html',
-            tables=[
-                final_table.to_html(
-                    classes="table",
-                    index=False,
-                    header="true"
-                ),
-                owner_table.to_html(
-                    classes="table",
-                    index=False,
-                    header="true"
-                ),
-            ]
+        "index.html",
+        tables=[
+            final_table.to_html(classes="table", index=False, header="true"),
+            owner_table.to_html(classes="table", index=False, header="true"),
+        ],
     )
